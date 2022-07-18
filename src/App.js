1506f6f721ddde1f6b9110c1ad8599998267b6ba
nasagaurav <nasa.gauravnasa@gmail.com>
2022-07-18 17:56:03 +0530

    Context-example2  completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,76 @@
-import React from "react";
-import "./style.css";
+import React {useState,useContext,createContext} from "react";
+
+const CounterContext = createContext();
 
 export default function App() {
+  const[x,setx] = useState(0);
+  const value = {x,
+    setx,
+  }
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <CounterContext.Provider value={value}>
+     <Increment />
+     <Decrement />
+     <Zero />
+     <Thousand />
+     <Twothousand />
+
+    </CounterContext.Provider>
   );
 }
+
+ const Increment=()=>{ 
+const data = useContext(counterContext);
+
+const p1=()=>{
+  data.setx(data.x+1)
+}
+
+  return (
+    <div>
+      <h3 onClick={p1}>Increment component: {data.x} </h3>
+    </div>
+  )
+ };
+  const Decrement=()=>{
+    const data=useContext(CounterContext);
+    const [x,setx] = data;
+    const p1 = ()=> setx(x-1);
+  return (
+    <div>
+      <h3 onClick={p1}>Decrement component :{x}</h3>
+    </div>
+  )
+  };
+
+  const Zero=()=>{
+    const useContext = CounterContext(0);
+    const [x,set]=data;
+    const p1= ()=> setx(0);
+
+  return (
+    <div>
+      <h3 onClick={p1}>Zero component:{x}</h3>
+    </div>
+  )
+  };
+ const Thousand=()=>{
+  const useContext = CounterContext(0);
+  const [x,set]=data;
+  const p1= ()=> setx(1000);
+ return (
+   <div>
+     <h3 onClick={p1}>Thousand component:{x}</h3>
+   </div>
+ )
+ };
+ const TwoThousand=()=>{
+  const useContext = CounterContext(0);
+  const [x,set]=data;
+  const p1= ()=> setx(2000);
+ return (
+   <div>
+     <h3 onClick={p1}>TwoThousand component:{x}</h3>
+   </div>
+ )
+ };
